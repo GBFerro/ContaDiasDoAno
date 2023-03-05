15aaf5faf4b398cd10d69d103a79398239db23db
GBFerro <giovani.ferro@usp.br>
2023-03-05 18:32:23 -0300

    Add project files.

---
+++ ContaDias/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int dias, ano1, ano2;
+
+        Console.WriteLine("Digite o primeiro ano: ");
+        ano1 = int.Parse(Console.ReadLine());
+
+        ano2 = ano1 - 1;
+
+        while (ano1 > ano2) { 
+        Console.WriteLine("Digite o segundo ano: ");
+        ano2 = int.Parse(Console.ReadLine());
+        }
+
+        dias = (ano2 - ano1 + 1)*365;
+        for (int i = ano1; i <= ano2; i++)
+        {
+            if (((i % 4 == 0) && (i % 100 != 0)) || (i % 400 == 0))
+            {
+                dias += 1;
+            }
+        }
+        Console.WriteLine("O total de dias é: " + dias);
+    }
+}
